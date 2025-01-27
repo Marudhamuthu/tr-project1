8298a0ac203173ebaee38320a07e21cc2916353f
Marudhamuthu <marudhamuthu@aagnia.com>
2025-01-27 21:47:49 +0530

    removing duplicate output

--- ec2.tf
+++ ec2.tf
@@ -59,12 +59,3 @@ resource "aws_backup_vault" "jenkins_vault" {
   name = "jenkins-backup-vault"
 }
 
-output "instance_id" {
-  description = "The ID of the Jenkins server instance."
-  value       = aws_instance.jenkins.id
-}
-
-output "public_ip" {
-  description = "The public IP of the Jenkins server."
-  value       = aws_eip.jenkins_eip.public_ip
-}
