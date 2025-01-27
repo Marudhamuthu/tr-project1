fb556d340dde6c43a6055bf00298e6c25eb50dbb
Marudhamuthu <marudhamuthu@aagnia.com>
2025-01-27 21:43:18 +0530

    docker compose file

--- variables.tf
+++ variables.tf
@@ -6,3 +6,7 @@ variable "sg-name" {}
 variable "instance-name" {}
 variable "key-name" {}
 variable "iam-role" {}
+variable "docker_compose_repo" {
+  description = "The GitHub repository URL containing the Docker Compose file for Jenkins."
+  type        = string
+}
